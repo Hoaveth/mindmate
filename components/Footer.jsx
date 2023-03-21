e2a3e2611310f208211253fc6ab0af54970166ef
Hoaveth <hoaveth.gaming@gmail.com>
2023-03-21 14:49:30 +0800

    Updated footer to be dynamic

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,21 +1,30 @@
 import Link from "next/link";
+import dynamic from "next/dynamic";
 import styles from "./styles/Footer.module.css";
 import { getLocalStorageItem } from "utils/common";
 import { USER_KEY } from "utils/constants";
+
+const DynamicLink = dynamic(
+  () => (
+    <Link
+      href="/"
+      className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
+    >
+      <span className={styles.stack}>Features</span>
+    </Link>
+  ),
+  {
+    ssr: false,
+  }
+);
 
 const Footer = () => {
   const user = getLocalStorageItem(USER_KEY);
 
   return (
     <div className={`${styles.container} footer bg-gray-700`}>
+      {user ? DynamicLink : null}
       <h1 className={styles.text}>
-        <Link
-          href="/"
-          className="block mt-4 lg:inline-block lg:mt-0 text-gray-200 hover:text-white mr-4"
-        >
-          <span className={styles.stack}>Features</span>
-        </Link>
-
         <a
           href="https://www.facebook.com/thepodbox"
           target="_blank"
