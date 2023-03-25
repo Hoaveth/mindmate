559961bc403cf3bb3a24d4bfeb2ad08d54adcd4b
junel <sacrojunel@gmail.com>
2023-03-25 12:44:14 +0800

    feat: remove the resize option in the text area (#8)
    add auto height with in minimum max height
    add overflow y for max height
    add auto scroll on buttom for the new chats

--- components/ChatBox.jsx
+++ components/ChatBox.jsx
@@ -1,5 +1,5 @@
 "use client";
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { GPT_CONDITIONER, GPT_MODEL, GPT_ROLE } from "utils/constants";
 import ChatLoad from "./ChatLoad";
 
@@ -11,6 +11,9 @@ const ChatBox = ({ option }) => {
   const [loading, setLoading] = useState(false);
   const [selectedOption, setSelectedOption] = useState();
   const [error, setError] = useState("");
+
+  const textareaRef = useRef();
+  const newMessageRef = useRef();
 
   useEffect(() => {
     setChats([]);
@@ -24,6 +27,33 @@ const ChatBox = ({ option }) => {
     };
     setUserMessage(messageObject);
   }, [message]);
+
+  //textarea
+  useEffect(() => {
+    if (textareaRef.current) {
+      textareaRef.current.style.height = "auto";
+      textareaRef.current.style.height = `${textareaRef.current?.scrollHeight}px`;
+
+      /**
+       * used to show scroll bar if greater than max height
+       * for changes make sure to change also the tailwind max height
+       *
+       * 200px = current max height
+       */
+      if (textareaRef.current?.scrollHeight > 200) {
+        textareaRef.current.style.overflowY = "scroll";
+      }
+    }
+  }, [message]);
+
+  //run side effect every new messages to scroll at the bottom
+  useEffect(() => {
+    scrollToBottom();
+  }, [chats]);
+
+  const scrollToBottom = () => {
+    newMessageRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
 
   const sendMessage = () => {
     if (message) {
@@ -71,10 +101,9 @@ const ChatBox = ({ option }) => {
     }
   };
 
-  console.log("error", error);
   return (
-    <div className="chatbox bg-white shadow-lg rounded-lg mt-10 p-6 max-w-lg mx-auto w-100">
-      <div className="flex flex-col gap-2">
+    <div className="chatbox bg-white shadow-lg rounded-lg mt-10 p-6 max-w-lg mx-auto w-100 relative flex flex-col">
+      <div className="flex flex-col gap-2 overflow-y-scroll">
         <div className="bg-gray-100 p-2 rounded-xl text-sm text-black">
           <strong>Instructions:</strong> {selectedOption?.description}
         </div>
@@ -91,35 +120,39 @@ const ChatBox = ({ option }) => {
               </div>
             );
           })}
-        {loading ? <ChatLoad /> : null}
+        {loading && <ChatLoad />}
         <span className="text-red-500 font-sans">{error}</span>
+        <div className="pb-14" ref={newMessageRef} />
       </div>
-      <div className="flex items-center mt-4">
-        <textarea
-          type="text"
-          className="border border-gray-300 rounded  px-4 py-2 w-full mr-2 h-20"
-          placeholder="Type your message here..."
-          onChange={(e) => setMessage(e.target.value)}
-          value={message}
-        />
-        <button
-          className={`${loading ? "bg-gray-500" : "bg-blue-500"} ${
-            loading ? "cursor-not-allowed" : "cursor-pointer"
-          } text-white rounded-full h-10 w-12 flex items-center justify-center`}
-          onClick={() => sendMessage()}
-          disabled={loading}
-        >
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="18"
-            height="18"
-            fill="currentColor"
-            className="bi bi-send-fill"
-            viewBox="0 0 16 16"
+      <div className="absolute bottom-0 left-0 w-full p-2 bg-white">
+        <div className="relative flex items-center mt-4 border border-gray-300 rounded-md py-2 shadow-md">
+          <textarea
+            ref={textareaRef}
+            type="text"
+            className="p-0 w-full pl-2 pr-4 whitespace-pre-wrap resize-none max-h-[200px] overflow-y-hidden focus:right-0 focus-visible:ring-offset-0 border-0 focus:outline-none focus:shadow-outline"
+            placeholder="Type your message here..."
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+          <button
+            className={`${loading ? "bg-gray-500" : "bg-blue-500"} ${
+              loading ? "cursor-not-allowed" : "cursor-pointer"
+            } text-white rounded-lg h-10 w-10 flex items-center justify-center absolute bottom-2 right-2`}
+            onClick={() => sendMessage()}
+            disabled={loading}
           >
-            <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />{" "}
-          </svg>
-        </button>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="18"
+              height="18"
+              fill="currentColor"
+              className="bi bi-send-fill"
+              viewBox="0 0 16 16"
+            >
+              <path d="M15.964.686a.5.5 0 0 0-.65-.65L.767 5.855H.766l-.452.18a.5.5 0 0 0-.082.887l.41.26.001.002 4.995 3.178 3.178 4.995.002.002.26.41a.5.5 0 0 0 .886-.083l6-15Zm-1.833 1.89L6.637 10.07l-.215-.338a.5.5 0 0 0-.154-.154l-.338-.215 7.494-7.494 1.178-.471-.47 1.178Z" />{" "}
+            </svg>
+          </button>
+        </div>
       </div>
     </div>
   );
